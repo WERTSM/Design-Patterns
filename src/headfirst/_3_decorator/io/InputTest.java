b4935910b99c7c3f87baa6fae8c718d7ca908c99
WERTSM <wertsm.work@gmail.com>
2017-02-07 16:16:48 +0300

    Pattern "Decorator"

--- src/headfirst/_3_decorator/io/InputTest.java
+++ src/headfirst/_3_decorator/io/InputTest.java
@@ -3,17 +3,17 @@ package headfirst._3_decorator.io;
  * Created by WERT on 06.02.2017.
  */
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class InputTest {
     public static void main(String[] args) throws IOException {
         int c;
 
         try {
-            InputStream in =
-                    new LowerCaseInputStream(
-                            new BufferedInputStream(
-                                    new FileInputStream("test.txt")));
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
 
             while ((c = in.read()) >= 0) {
                 System.out.print((char) c);
