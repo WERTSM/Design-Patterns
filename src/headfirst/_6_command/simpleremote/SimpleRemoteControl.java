8bb6ee1823843bdfb70ac0ea9d5eeee27990f3c2
WERT <wert.java@gmail.com>
2017-02-09 20:48:32 +0300

    Pattern "Command"

---
+++ src/headfirst/_6_command/simpleremote/SimpleRemoteControl.java
@@ -0,0 +1,18 @@
+package headfirst._6_command.simpleremote;
+
+//
+// This is the invoker
+//
+public class SimpleRemoteControl {
+	Command slot;
+ 
+	public SimpleRemoteControl() {}
+ 
+	public void setCommand(Command command) {
+		slot = command;
+	}
+ 
+	public void buttonWasPressed() {
+		slot.execute();
+	}
+}
