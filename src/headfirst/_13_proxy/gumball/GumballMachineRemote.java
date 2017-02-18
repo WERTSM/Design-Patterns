e13b59f922aa730c1dfdde0f55991c0ef992f7bc
WERT <wert.java@gmail.com>
2017-02-18 21:20:23 +0300

    Pattern "Proxy"

---
+++ src/headfirst/_13_proxy/gumball/GumballMachineRemote.java
@@ -0,0 +1,9 @@
+package headfirst._13_proxy.gumball;
+
+import java.rmi.*;
+ 
+public interface GumballMachineRemote extends Remote {
+	public int getCount() throws RemoteException;
+	public String getLocation() throws RemoteException;
+	public State getState() throws RemoteException;
+}
